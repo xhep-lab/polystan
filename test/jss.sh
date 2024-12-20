6334fc6092f3fbb9f2414012866750c732fa077d
Andrew Fowlie <andrew.j.fowlie@gmail.com>
2024-12-20 16:16:29 +0800

    start jss reproducer example

---
+++ test/jss.sh
@@ -0,0 +1,23 @@
+# build everything required for this model
+
+make PS_MODEL=./examples/bernoulli.stan  
+
+# run model with fixed seeds
+
+./build/bernoulli/run data --file examples/bernoulli.data.json random --seed=11 polychord --seed=12 --overwrite
+
+# check evidence estimate etc
+
+cat chains/bernoulli.stats
+
+# check log evidence value
+
+log_evidence=$(grep -Po '"log evidence": .*?[^\\],' bernoulli.json)
+
+expected='"log evidence": -6.186195373535156,'
+
+echo "$log_evidence" versus "$expected"
+
+if [ "$log_evidence" != "$expected" ]; then
+  exit 1
+fi
