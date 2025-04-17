7a2b1f3735fd17fee1f8e10e23dba6bbe3a58595
Andrew Fowlie <andrew.j.fowlie@gmail.com>
2025-04-17 11:28:52 +0800

    add missing header

---
+++ src/polystan/test.hpp
@@ -0,0 +1,14 @@
+#ifndef POLYSTAN_TEST_HPP_
+#define POLYSTAN_TEST_HPP_
+
+#include "read.hpp"
+
+namespace polystan {
+namespace test {
+
+double insertion_index_p_value(const std::string& basename) { return 0.; }
+
+}  // end namespace test
+}  // end namespace polystan
+
+#endif  // POLYSTAN_TEST_HPP_
