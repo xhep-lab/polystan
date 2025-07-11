599b1e47abb02f0f58b3ae0d9f4c3e1ec608a5eb
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-07-11 16:40:10 +0800

    expand instructions

--- contrib/jss.sh
+++ contrib/jss.sh
@@ -1,39 +1,48 @@
-# First, we install necessary dependencies for building on Ubuntu/debian:
+# This is a guide to reproducing examples from the paper on a
+# Ubuntu/debian system
+
+# First, we install necessary dependencies
 
 sudo apt-get install git make gcc gfortran libopenmpi-dev
 
-# We clone the source code recursively to obtain the code and submodules:
+# We clone the source code recursively to obtain the code and submodules
 
 git clone --recursive https://github.com/xhep-lab/polystan
 
-# We can now compile a simple example:
+# We can now compile a simple example
 
 cd polystan
 make examples/bernoulli
 
-# and run the resulting executable:
+# and run the resulting executable
 
 ./examples/bernoulli data --file examples/bernoulli.data.json
 
 # Now we reproduce results from the paper. This requires some Python 
-# dependencies. We will use a virtual environment:
+# dependencies. We will use a virtual environment
 
 sudo apt install python3 python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r contrib/requirements.txt
 
-# Now we reproduce figure 2 by creating "disaster.pdf" from a Python script
+# Now we reproduce figure 2 from a Python script
 
 python3 contrib/plot_disaster.py
 
+# This creates "disaster.pdf". Take a look
+
+xdg-open disaster.pdf
+
 # Now we reproduce comparisons of bridge sampling and polystan that appear in
-# Section 6 Examples from a Python script
+# Section 6 Examples from a Python script. This command may take some time
 
 python3 contrib/compare.py
 
-# This creates a data file "compare.json"
+# This creates a data file "compare.json". Take a look
 
-# Lastly, to run the unit tests
+more compare.json
+
+# Lastly, we can run the unit tests
 
 python3 -m pytest .
